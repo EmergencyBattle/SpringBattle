84495b58da0625eef964e696b9cd4efa3b0303a0
Emilia <sugarcubehere@gmail.com>
2018-04-11 15:32:24 +0200

    Opening and closing menu now works

--- menuscript.js
+++ menuscript.js
@@ -23,20 +23,46 @@ function menuMove() {
     console.log("the function has started");
     menu.classList.add("showmenulist");
     menu.style.bottom = "270px";
+    if (menu.classList.contains("hidemenulist")) {
+        menu.classList.remove("hidemenulist");
+        console.log("removed hidemenulist");
+    } else {
+        console.log("no hidemenulist to remove");
+    }
 }
 
 function menuReverse() {
     console.log("the function has started");
     menu.classList.add("hidemenulist");
     menu.style.bottom = "-80px";
+    if (menu.classList.contains("showmenulist")) {
+        menu.classList.remove("showmenulist");
+        console.log("removed showmenulist");
+    } else {
+        console.log("no showmenulist to remove");
+    }
 }
 
 function backgroundMove() {
     console.log("the function has started");
-
+    background.classList.add("showmenuimage");
+    background.style.bottom = "0px";
+    if (background.classList.contains("hidemenuimage")) {
+        background.classList.remove("hidemenuimage");
+        console.log("removed hidemenuimage");
+    } else {
+        console.log("no hidemenuimage to remove");
+    }
 }
 
 function backgroundReverse() {
     console.log("the function has started");
-
+    background.classList.add("hidemenuimage");
+    background.style.bottom = "-350px";
+    if (background.classList.contains("showmenuimage")) {
+        background.classList.remove("showmenuimage");
+        console.log("removed showmenuimage");
+    } else {
+        console.log("no showmenuimage to remove");
+    }
 }
